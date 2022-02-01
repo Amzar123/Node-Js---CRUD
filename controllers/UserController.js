b1016906cb04ba75a5f9d020095b42ef81433c5b
Amzar123 <aji.muhammad.zapar080500@gmail.com>
2022-02-01 21:34:42 +0700

    update API

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,6 +3,21 @@ const bcrypt = require('bcryptjs')
 
 //READ
 const index = (req,res,next)=>{
+    UserObj.findById(req.body.userId)
+    .then(respone=>{
+        res.json({
+            respone
+        })
+    })
+    .catch(error => {
+        res.json({
+            message: error
+        })
+    })
+}
+
+//READ
+const getAll = (req,res,next)=>{
     UserObj.find()
     .then(respone=>{
         res.json({
@@ -49,24 +64,33 @@ const store = (req,res,next) => {
 
 //UPDATE
 const updateUser = (req,res,next)=>{
-    let userId = req.body.userId
-    let updateData = {
-        nama : req.body.nama,
-        alamat : req.body.alamat,
-        username : req.body.username, 
-        password : req.body.password,
-        role : req.body.role
-    }
 
-    UserObj.findByIdAndUpdate(userId, {$set: updateData})
-    .then(()=>{
-        res.json({
-            message: 'user berhasil di update'
+    let userId = req.body.userId
+
+    bcrypt.hash(req.body.password, 10, function(err, hashedPass){
+        if (err){
+            res.json({
+                error:err
+            })
+        }
+
+        let UserObj = new UserObj({
+            nama: req.body.nama,
+            username: req.body.username,
+            alamat : req.body.alamat,
+            password: hashedPass,
+            role:req.body.role
         })
-    })
-    .catch(error => {
-        res.json({
-            message: error
+        UserObj.findByIdAndUpdate(userId, {$set: updateData})
+        .then(()=>{
+            res.json({
+                message: 'user berhasil di update'
+            })
+        })
+        .catch(error => {
+            res.json({
+                message: error
+            })
         })
     })
 }
@@ -88,5 +112,5 @@ const deleteUser = (req,res,next)=>{
 }
 
 module.exports ={
-    index,store, updateUser, deleteUser
+    index,store, updateUser, deleteUser, getAll
 }
--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,8 @@ const UserController = require('../controllers/UserController')
 const autentikasi = require('../middleware/Autentikasi')
 
 
-router.get('/',autentikasi.autentikasi,autentikasi.checkRole(['admin','user']),UserController.index)
+router.post('/',autentikasi.autentikasi,autentikasi.checkRole(['admin','user']),UserController.index)
+router.get('/all-data', autentikasi.autentikasi,autentikasi.checkRole(['admin']),UserController.getAll)
 router.post('/add', autentikasi.autentikasi,autentikasi.checkRole(['admin']),UserController.store)
 router.put('/update',autentikasi.autentikasi,autentikasi.checkRole(['admin']), UserController.updateUser)
 router.delete('/delete',autentikasi.autentikasi,autentikasi.checkRole(['admin']), UserController.deleteUser)
